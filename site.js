acccb3de66c753a61f7e90ba2294b7b82252cddf
Ismael Rodriguez Ferreras <300084203@fvtc.edu>
2024-11-05 21:44:02 -0600

    Added Welcome div

---
+++ site.js
@@ -0,0 +1,20 @@
+var message = document.querySelector('#welcome')
+
+const hours = new Date().getHours()
+
+const isMorning= hours >= 4 && hours < 12
+const isAfternoon = hours >= 12 && hours < 17
+const isEvening = hours >= 17 || hours < 4
+
+if (isMorning) 
+{
+    message.textContent = "Good Morning, Welcome to Ismael's Page"
+}
+else if (isAfternoon) 
+{
+    message.textContent = "Good Afternoon, Welcome to Ismael's Page"
+}
+else if (isEvening) 
+{
+    message.textContent = "Good Evening, Welcome to Ismael's Page"
+ }
